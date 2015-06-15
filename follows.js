b38ea02401454ab09614791d378a25a6903c907b
adigirolamo <digirolamo18@gmail.com>
2015-06-15 15:26:09 +0200

    Update follows.js

--- follows.js
+++ follows.js
@@ -13,7 +13,7 @@
 	*/
 
 	var defaultKey		= 'LCZod3mfQkCwmtR1hBvTmHGhoqhd91WeTrNJdTAXbP2xmNm5', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [37578880], // Comma separated array of Xively Feed ID numbers
+		defaultFeeds	= ['37578880'], // Comma separated array of Xively Feed ID numbers
 		applicationName	= 'IL MIO LOGO', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
